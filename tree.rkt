5cef7eda88c3ce25794eeeee47f2879884bd1ed3
desmond0331 <wangdaiyang@vip.qq.com>
2016-05-13 02:03:43 -0400

    Update tree.rkt

--- tree.rkt
+++ tree.rkt
@@ -1,5 +1,9 @@
 ;; this is a racket file
+#lang racket
 (define (hello-world str)
 (cond
-[(string=? "Hello World!") true]
+[(string=? "Hello World!" str) true]
 [else false]))
+
+(equal? (hello-world "string") false)
+(equal? (hello-world "Hello World!") true)
